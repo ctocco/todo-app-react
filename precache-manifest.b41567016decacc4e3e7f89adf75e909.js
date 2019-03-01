7ba7c115b3ae32f09779cec2e5f0edc54e031af9
Claudio Tocco <tocco.a.claudio@gmail.com>
2019-03-01 11:52:12 +0100

    Updates

---
+++ precache-manifest.b41567016decacc4e3e7f89adf75e909.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fd6cc2ae071a7c92c49f",
+    "url": "/todo-app-react/static/css/main.558168ab.chunk.css"
+  },
+  {
+    "revision": "fd6cc2ae071a7c92c49f",
+    "url": "/todo-app-react/static/js/main.fd6cc2ae.chunk.js"
+  },
+  {
+    "revision": "71507c0fe40142d1c314",
+    "url": "/todo-app-react/static/js/runtime~main.71507c0f.js"
+  },
+  {
+    "revision": "b6c7537494504177f087",
+    "url": "/todo-app-react/static/js/2.b6c75374.chunk.js"
+  },
+  {
+    "revision": "6ed91ba9db89052306899bec44c6a905",
+    "url": "/todo-app-react/index.html"
+  }
+];
