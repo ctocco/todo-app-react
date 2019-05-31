af1fc93ac2ef660006b511f4f8eed8c0ca38395b
Claudio Tocco <tocco.a.claudio@gmail.com>
2019-05-31 18:50:37 +0200

    changing look of todo link

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,15 +6,14 @@ const Home = () => {
     <div className="container center" id="header">
       <div className="row center">
         <div className="col s12">
-          <div className="card white darken-1">
-            <div className="card-content white-text" />
+          <div className="card">
             <div className="card-title">
               <h1>Welcome!</h1>
             </div>
             <p>Please click on the link below to go to go to your Todo list</p>
-            <div className="card-action center waves-effect">
+            <div className="center todo-homepage-link">
               <NavLink id="todo-nav" to="/todos">
-                Todo
+                <h5>Todo</h5>
               </NavLink>
             </div>
           </div>
